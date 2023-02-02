684737475d976640caea21fdc5d6aee8b5c1cb23
nyame100 <nyamerichmond100@gmail.com>
2023-02-02 15:27:18 +0100

    my first commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  return (
+    <section className="section serach">
+      <form action="" className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="">search your favourite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum sed illo
+        reprehenderit inventore recusandae maxime! Pariatur ad, quo obcaecati,
+        officiis sint aut adipisci tempora dolor modi saepe, iste sequi beatae
+        nesciunt asperiores odio esse accusamus quos nemo doloremque. Sed
+        perferendis blanditiis quo, repudiandae veritatis sunt nobis odit
+        reprehenderit animi aperiam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
